57c4a922cd86c875e365241763a822a843bd17d3
Diego Rosales <rdiegovladimir@gmail.com>
2024-03-23 17:32:05 -0600

    feat: upgrade storybook

--- package/.storybook/main.ts
+++ package/.storybook/main.ts
@@ -3,7 +3,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../lib/**/*.mdx", "../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
--- package/.storybook/preview.ts
+++ package/.storybook/preview.ts
@@ -1,16 +1,8 @@
 import type { Preview } from "@storybook/react";
-import "../src/index.css";
+import "../lib/index.css";
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+  parameters: {},
 };
 
 export default preview;
