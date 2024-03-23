ca6e903467d91282660d71fa534a798961304c6d
Diego Rosales <rdiegovladimir@gmail.com>
2024-03-23 17:08:13 -0600

    feat: prepare for deployment

--- package/vite.config.ts
+++ package/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ include: ["lib"] })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    emptyOutDir: true,
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
